c2d9b9c96b22fbc10286b441d54745a70898a1be
chris.kuo <126221813+beyondcars@users.noreply.github.com>
2024-02-19 14:47:06 +0800

    init project

---
+++ src/app.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '@/styles/index.css'
+
+const App = () => {
+
+    return (
+        <>
+            <div className="header"></div>
+            <div className="content"></div>
+            <div className="footer"></div>
+        </>
+    )
+}
+
+export default App;
