e6094e0712eea61dd535e23a114e0d7d10f0627d
chris.kuo <126221813+beyondcars@users.noreply.github.com>
2024-03-05 11:41:53 +0800

    custom hook, Container and presentation patterns

--- src/app.tsx
+++ src/app.tsx
@@ -1,41 +1,47 @@
 import React from 'react'
 import '@/styles/index.css'
-import CharactersContainer from './components/CharactersContainer'
-import Auth from './components/Auth'
-import Header from './components/Header'
-import Avatar from './components/Avatar'
-import withLoading from './hoc/withLoading'
-import Toggle from './components/Toggle'
-import CustomDemo from './components/CustomDemo'
+// import CharactersContainer from './components/CharactersContainer'
+// import Auth from './components/Auth'
+// import Header from './components/Header'
+// import Avatar from './components/Avatar'
+// import withLoading from './hoc/withLoading'
+// import Toggle from './components/Toggle'
+// import CustomDemo from './components/CustomDemo'
+
+import Ex1 from './example/Ex1';
 
 const App = () => {
 
-    const AvatarWithLoading = withLoading(Avatar)
+    // const AvatarWithLoading = withLoading(Avatar)
+
+    // return (
+    //     <>
+    //         <Header />
+    //         <div className="content">
+    //             {/* <CharactersContainer /> */}
+    //             {/* <Auth /> */}
+    //             {/* <AvatarWithLoading name='orange' loading={true} /> */}
+    //             <div className='section'>
+    //                 <Toggle>
+    //                     <Toggle.On>
+    //                         <div>ON</div>
+    //                     </Toggle.On>
+    //                     <Toggle.Off>
+    //                         <div>OFF</div>
+    //                     </Toggle.Off>
+    //                     <Toggle.Button text='toggle' />
+    //                 </Toggle>
+    //             </div>
+    //             <div className='section'>
+    //                 <CustomDemo />
+    //             </div>
+    //         </div>
+    //         <div className="footer"></div>
+    //     </>
+    // )
 
     return (
-        <>
-            <Header />
-            <div className="content">
-                {/* <CharactersContainer /> */}
-                {/* <Auth /> */}
-                {/* <AvatarWithLoading name='orange' loading={true} /> */}
-                <div className='section'>
-                    <Toggle>
-                        <Toggle.On>
-                            <div>ON</div>
-                        </Toggle.On>
-                        <Toggle.Off>
-                            <div>OFF</div>
-                        </Toggle.Off>
-                        <Toggle.Button text='toggle' />
-                    </Toggle>
-                </div>
-                <div className='section'>
-                    <CustomDemo />
-                </div>
-            </div>
-            <div className="footer"></div>
-        </>
+        <Ex1 />
     )
 }
 
---
+++ src/example/Ex1.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CharactersContainer from '../components/CharactersContainer'
+
+const Ex1 = () => {
+  return (
+    <div>
+      <CharactersContainer />
+    </div>
+  )
+}
+
+export default Ex1
